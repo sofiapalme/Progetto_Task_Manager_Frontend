bdb59ee3bf1680fa8d5b490d3cd93cb50046023a
DiegoLoCicero <diegolocicero9@gmail.com>
2025-11-14 15:49:00 +0100

    Added style to login component and dashboard component

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,32 +1,65 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class DashboardComponent {
   email: string | null = '';
+  username: string | null = '';
 
+  modalOpen = false;
 
-  constructor(private http: HttpClient) { }
+  formData = {
+    titolo: '',
+    descrizione: '',
+    fasi: '',
+    collaboratori: ''
+  };
 
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.email = localStorage.getItem('email');
+
+    if (this.email) {
+      this.http.get<any>(`http://localhost:8080/users/${this.email}`).subscribe({
+        next: (res) => {
+          this.username = res.username;
+          this.email = res.email;
+        },
+        error: (err) => console.error('Errore recupero utente:', err)
+      });
+    }
   }
 
+  openModal() {
+    this.modalOpen = true;
+  }
 
-  aggiungiProgetto() {
-    this.http.post('/api/progetti', {}).subscribe({
-      next: (res) => console.log('Progetto aggiunto', res),
-      error: (err) => console.error('Errore:', err)
+  closeModal() {
+    this.modalOpen = false;
+  }
+
+  submitProgetto() {
+    if (!this.formData.titolo || !this.formData.fasi) {
+      alert("Titolo e Fasi sono obbligatori");
+      return;
+    }
+
+    this.http.post('http://localhost:8080/test', this.formData).subscribe({
+      next: () => {
+        alert("Progetto inviato!");
+        this.closeModal();
+        this.formData = { titolo: '', descrizione: '', fasi: '', collaboratori: '' };
+      },
+      error: (err) => console.error("Errore invio:", err)
     });
   }
 }
